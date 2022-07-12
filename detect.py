32e96df9fe489f54be8fb362ac8cbbb3d70535a0
TALHA_LOUBNA <talha_loubna@emsi-edu.ma>
2022-07-12 11:48:53 +0100

    changes

--- detect.py
+++ detect.py
@@ -14,8 +14,9 @@ while cam.isOpened():
             continue
         x, y, w, h = cv2.boundingRect(c)
         cv2.rectangle(frame1, (x, y), (x+w, y+h), (0, 255, 0), 2)
+        cv2.putText(frame1,"Status:{}".format('Movement'),(10,20),cv2.FONT_HERSHEY_SIMPLEX,1,(0,0,255),3)
     if cv2.waitKey(10) == ord('q'):
         break
     cv2.imshow('My Cam', frame1)
 
-    
+cv2.destroyAllWindows()
